5fbac49e63d76f2c788ffcffade9a237c8dfe2f0
Jade <jadejonescrypto@gmail.com>
2025-07-24 15:26:28 +0100

    test

---
+++ python_projects/project_1/quiz_game.py
@@ -0,0 +1,36 @@
+print("Welcome to my computer quiz")
+
+playing = input('Do you want to play? ')
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's Play :)")
+
+answer = input("What does cpu stand for? ")
+if answer.lower() == "central processing unit":
+    print("Correct")
+else:
+    print("Incorrect!")
+
+answer = input("What does gpu stand for? ")
+
+if answer.lower() == "graphics processing unit":
+    print("Correct")
+else:
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print("Correct")
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print("Correct")
+else:
+    print("Incorrect!")
+
+print("Your score is")
+

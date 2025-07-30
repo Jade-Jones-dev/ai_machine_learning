8a5b750e13063956f5ada493f804ef605e5da898
Jade <jadejonescrypto@gmail.com>
2025-07-30 14:53:19 +0100

    fastapi tutorial completed

--- week_3/fastapi_tutorial/main.py
+++ week_3/fastapi_tutorial/main.py
@@ -1,24 +1,29 @@
 from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 items = []
+
+class Item(BaseModel):
+    text: str 
+    is_done: bool = False
 
 @app.get('/')
 def root():
     return{'Hello' : 'World'}
 
 @app.post('/items')
-def create_item(item: str):
+def create_item(item: Item):
     items.append(item)
     return items
 
-@app.get('/items')
+@app.get('/items', response_model=list[Item])
 def list_items(limit: int = 10):
     return items[0:limit]
 
-@app.get('/items/{item_id}')
-def get_item(item_id: int) -> str:
+@app.get('/items/{item_id}', reponse_model=Item)
+def get_item(item_id: int) -> Item:
     if item_id < len(items):
         return items[item_id]
     else:
--- week_3/fastapi_tutorial/notes.md
+++ week_3/fastapi_tutorial/notes.md
@@ -7,7 +7,10 @@ start the server
 uvicorn main:app --reload
 
 post to the server
-curl -X POST -H "Content-Type": application/json" 'http://127/0.0.1:8000/items?item=apple'
+curl -X POST -H "Content-Type: application/json" -d '{"text" : "apple"}' "http://127/0.0.1:8000/items"
 
 get item from server
-curl -X GET http://127.0.0.1:8000/items/0
+curl -X GET 'http://127.0.0.1:8000/items/0'
+
+get items
+curl -X GET 'http://127.0.0.1:8000/items'

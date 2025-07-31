dab0b9a694c048b9a054f559c6a53879c716d475
Jade <jadejonescrypto@gmail.com>
2025-07-31 18:43:38 +0100

    pig game completed

--- python_projects/project_6/pig_game.py
+++ python_projects/project_6/pig_game.py
@@ -29,6 +29,8 @@ player_scores = [0 for _ in range(players)]
 while max(player_scores) < max_score:
 
     for player_idx in range(players):
+        print("\nPlayer", player_idx + 1, "turn has started")
+        print("Your total score is:", player_scores[player_idx], "\n")
         current_score = 0
 
         while True:
@@ -40,8 +42,17 @@ while max(player_scores) < max_score:
             value = roll()
             if value == 1:
                 print("You rolled a 1 turn done")
+                current_score = 0
                 break
             else:
+                current_score += value
                 print("You rolled a:", value)
 
             print("Your score is:", current_score)
+
+        player_scores[player_idx] += current_score
+        print("Your current score is:", player_scores[player_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1, "is the winner with a score of", max_score)

257a7c864bc8064fd0f52c70ce847f10b74cc736
Jade <jadejonescrypto@gmail.com>
2025-08-05 14:42:40 +0100

    changed pairs for colors

--- python_projects/11_typing_test.py
+++ python_projects/11_typing_test.py
@@ -14,17 +14,25 @@ def wpm_test(stdscr):
     target_text = "Hello world this is test text for this app"
     current_text = []
 
-    stdscr.clear()
-    stdscr.addstr(target_text)
-    stdscr.refresh()
-    stdscr.getkey()
+    while True:
+        key = stdscr.getkey()
+        current_text.append(key)
+
+        stdscr.clear()
+        stdscr.addstr(target_text)
+
+
+        for char in current_text:
+            stdscr.addstr(char, curses.color_pair(1))
+        
+        stdscr.refresh()
 
 
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
-    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
+    curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
     
     start_screen(stdscr)
     wpm_test(stdscr)

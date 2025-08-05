23495a7115f36e2dbf0cc76f747af82c44593908
Jade <jadejonescrypto@gmail.com>
2025-08-05 14:35:08 +0100

    fixed bug with displaying text

--- python_projects/11_typing_test.py
+++ python_projects/11_typing_test.py
@@ -1,14 +1,34 @@
 import curses
 
 from curses import wrapper
+stdscr = curses.initscr()
+
+def start_screen(stdscr):
+    stdscr.clear()
+    stdscr.addstr("Welcome to your speed typing test")
+    stdscr.addstr("\nPress any key to start!")
+    stdscr.refresh()
+    stdscr.getkey()
+
+def wpm_test(stdscr):
+    target_text = "Hello world this is test text for this app"
+    current_text = []
+
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh()
+    stdscr.getkey()
+
+
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    stdscr.clear()
-    stdscr.addstr("hello world")
-    stdscr.refresh()
-    stdscr.getkey()
+    
+    start_screen(stdscr)
+    wpm_test(stdscr)
+
+
 
 wrapper(main)

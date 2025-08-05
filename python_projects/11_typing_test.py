9a1c488aed60384149af22999f3e144f9a0cc6e1
Jade <jadejonescrypto@gmail.com>
2025-08-05 15:11:59 +0100

    added quit option with ord

--- python_projects/11_typing_test.py
+++ python_projects/11_typing_test.py
@@ -1,7 +1,9 @@
 import curses
 
 from curses import wrapper
+
 stdscr = curses.initscr()
+
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -10,33 +12,35 @@ def start_screen(stdscr):
     stdscr.refresh()
     stdscr.getkey()
 
+
 def wpm_test(stdscr):
     target_text = "Hello world this is test text for this app"
     current_text = []
 
     while True:
-        key = stdscr.getkey()
-        current_text.append(key)
-
         stdscr.clear()
         stdscr.addstr(target_text)
 
-
         for char in current_text:
             stdscr.addstr(char, curses.color_pair(1))
-        
+
         stdscr.refresh()
 
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
+        current_text.append(key)
 
 
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
-    
+
     start_screen(stdscr)
     wpm_test(stdscr)
-
 
 
 wrapper(main)

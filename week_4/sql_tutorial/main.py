7e568d9a2a8ab9f43b201062d4046cb6008f45f6
Jade <jadejonescrypto@gmail.com>
2025-08-06 16:05:00 +0100

    steps for database

---
+++ week_4/sql_tutorial/main.py
@@ -0,0 +1,14 @@
+#imports
+
+
+# 1 - setup/ initialize database
+
+# 2 - create a table in the database
+
+# 3 - add a user to the database
+
+# 4 - query all users in the database
+
+# 5 - delete a user from the database
+
+# 6 - update an existing user

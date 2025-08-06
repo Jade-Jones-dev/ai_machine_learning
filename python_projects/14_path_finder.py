c62f2778057a28ff744288e1c322e4cf02ce31b8
Jade <jadejonescrypto@gmail.com>
2025-08-06 15:01:11 +0100

    finding position of start of maze

--- python_projects/14_path_finder.py
+++ python_projects/14_path_finder.py
@@ -26,6 +26,21 @@ def print_maze(maze, stdscr, path=[]):
             stdscr.addstr(i, j* 2, value, BLUE)
 
 
+
+def find_start(maze, start):
+    for i, row in enumerate(maze):
+        for j, value in enumerate(row):
+            if value == start:
+                return i, j
+            
+    return None
+
+def find_path(maze, stdscr):
+    start = "O"
+    end = "X"
+    start_pos = find_start(maze, start)
+
+
 def main(stdscr):
     curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
     curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)

48d3302471f4bde6d5087ea3ff1e866272e6a465
Jade <jadejonescrypto@gmail.com>
2025-08-06 12:35:28 +0100

    wpm test completed

--- python_projects/11_typing_test.py
+++ python_projects/11_typing_test.py
@@ -1,4 +1,6 @@
 import curses
+import time
+import random
 
 from curses import wrapper
 
@@ -24,20 +26,35 @@ def display_text(stdscr, target, current, wpm=0):
             color = curses.color_pair(2)
         stdscr.addstr(0, i, char, color)
 
-
+def load_text():
+    with open("test.txt", "r") as f:
+        lines = f.readlines()
+        return random.choice(lines).strip()
 
 
 def wpm_test(stdscr):
-    target_text = "Hello world this is test text for this app"
+    target_text = load_text()
     current_text = []
     wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
     while True:
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
         stdscr.clear()
         display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
 
         for char in current_text:
             stdscr.addstr(char, curses.color_pair(1))
@@ -58,7 +75,15 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the test. Press any key to continue")
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
+
 
 
 wrapper(main)

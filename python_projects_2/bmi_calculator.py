c8bd11bac690878a52d8bef5c515801e488a306c
Jade <jadejonescrypto@gmail.com>
2025-08-11 18:27:04 +0100

    Added error handling for bmi calculator

--- python_projects_2/bmi_calculator.py
+++ python_projects_2/bmi_calculator.py
@@ -1,8 +1,18 @@
 # BMI calculator
 
 def get_bmi(height, weight):
-    height = float(height)
-    weight = float(weight)
+    try:
+        height = float(height)
+        weight = float(weight)
+    except ValueError:
+        print("Please enter a valid value")
+        return None
+
+    if height <= 0 or weight <= 0:
+        print("Height and weight must be positive values")
+        return None
+
+
     bmi = weight / (height * height)
     bmi = round(bmi, 1)
     print(f"Your bmi is {bmi}")
@@ -23,7 +33,8 @@ if __name__ == "__main__":
     height = input("What is your height in meters (eg 1.75): ")
     weight = input("What is your weight in kg? ")
     bmi = get_bmi(height, weight)
-    get_bmi_int(bmi)
+    if bmi is not None:
+        get_bmi_int(bmi)
 
 
 

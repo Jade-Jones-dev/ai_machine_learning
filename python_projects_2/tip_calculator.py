9122c3f04c7db6b49ac51cd71a8da003adb2a304
Jade <jadejonescrypto@gmail.com>
2025-08-13 16:06:35 +0100

    tip calculator logic

--- python_projects_2/tip_calculator.py
+++ python_projects_2/tip_calculator.py
@@ -1,5 +1,16 @@
 # tip calculator
 
-# get value of order
-# find out percentage of tip
-# return tip amount and total
+def calculate_tip(order_value, tip):
+    order_value = float(order_value)
+    tip = float(tip)
+    tip_amount = (order_value / 100) * tip
+    total_bill = order_value + tip_amount
+    print(
+        f"Your tip amount is {tip_amount}. Your total bill including tip is{total_bill}"
+    )
+
+
+if __name__ == "__main__":
+    order_value = input("What is value of your order (eg 152.95): ")
+    tip = input("What percentage would you like to tip? ")
+    calculate_tip(order_value, tip)
